a2a0754ca96af3cd619599daaa829540fbc85ac2
Dan Gilday <dangitday@gmail.com>
2017-04-22 17:50:45 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,214 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var synth = window.speechSynthesis;
+
+var grammar = '#JSGF V1.0; grammar jokes; public <joke> = joke;';
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+// recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+
+var jokeObjects = [
+  {
+    'title': '',
+    'joke': 'Tech Support: "I need you to right-click on the Open Desktop." \nCustomer: "Ok." \nTech Support: "Did you get a pop-up menu?"Customer: "No." \nTech Support: "Ok. Right click again. Do you see a pop-up menu?" \nCustomer: "No." \nTech Support: "Ok, sir. Can you tell me what you have done up until this point?" \nCustomer: "Sure, you told me to write \'click\' and I wrote click\'."',
+    'category': 'Computer',
+    'rating': 1,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'You know it is time to reassess your relationship with \nyour computer when....\n\n1. You wake up at 4 O\'clock in the morning to go to the bathroom and \nstop to check your email on the way back to bed.\n\n2. You turn off your computer and get an awful empty feeling, as if \nyou just pulled the plug on a loved one.\n\n3. You decide to stay in college for an additional year or two, just \nfor the free internet access.\n\n4. You laugh at people with 28.8 modems.\n\n5. You start using smileys :-) in your snail mail.\n\n6. You find yourself typing "com" after every period when using a word \nprocessor.com\n\n7. You can\'t correspond with your mother because she doesn\'t have a \ncomputer.\n\n8. When your email box shows "no new messages" and you feel reall',
+    'category': 'Computer',
+    'rating': 1,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'The boy is smoking and leaving smoke rings into the air.\nThe girl gets irritated with the smoke and says to her lover: "Can\'t you see the warning written on the cigarettes packet, smoking is injurious to health!"\n\nThe boy replies back: "Darling, I am a programmer. We don\'t worry about warnings, we only worry about errors."',
+    'category': 'Computer',
+    'rating': 1,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Why do you think I spend too much time at my computer? \n\n- Well, dear... Every time I ask you to close the windows you answer with "Please wait while your computer shuts down"...',
+    'category': 'Computer',
+    'rating': 0.9199999999999998,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'It says: "Press Any Key"\nIt means: "Press any key you like but I\'m not moving."\n\nIt says: "Press A Key"\n(This one\'s a programmers joke. Nothing happens unless you press the "A" key.)\n\nIt says: "Fatal Error. Please contact technical support quoting error\nno. 1A4-2546512430E" It means: "... where you will be kept on hold for 10 minutes, only to be told that it\'s a hardware problem."\n\nIt says: "Installing program to C:...."\nIt means: "... And I\'ll also be writing a few files into c:windows and c:windowssystem where you\'ll NEVER find them."\n\nIt says: "Please insert disk 11"\nIt means: "Because I know darn well there are only 10 disks."\n\nIt says: "Not enough memory"\nIt means: "I don\'t CARE',
+    'category': 'Computer',
+    'rating': 0.9,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Q. How does Bill Gates enter his house?\nA. He uses "windows".',
+    'category': 'Computer',
+    'rating': 0.8800000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'There was once a young man who, in his youth, professed his desire to become a great writer.\n\nWhen asked to define "great" he said, "I want to write stuff that the whole world will read, stuff that people will react to on a truly emotional level, stuff that will make them scream, cry, howl in pain and anger!"\n\nHe now works for Microsoft, writing error messages.',
+    'category': 'Computer',
+    'rating': 0.8800000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'A programmer was walking along the beach when he found a lamp. Upon rubbing the lamp a genie appeared who stated "I am the most powerful genie in the world. I can grant you any wish you want, but only one wish."\n\nThe programmer pulled out a map of the Mediterranean area and said "I\'d like there to be a just and last peace among the people in the middle east."\n\nThe genie responded, "Gee, I don\'t know. Those people have been fighting since the beginning of time. I can do just about anything, but this is beyond my limits."\n\nThe programmer then said, "Well, I am a programmer and my programs have a lot of users. Please make all the users satisfied with my programs, and let them ask sensible changes"\n\nGenie: "Uh, let me see that map again."',
+    'category': 'Computer',
+    'rating': 0.8800000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Once a programmer drowned in the sea. Many Marines where at that time on the beach, but the programmer was shouting "F1 F1" and nobody understood it.',
+    'category': 'Computer',
+    'rating': 0.8800000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Q. How does Bill Gates enter his house?\nA. He uses "windows".',
+    'category': 'Computer',
+    'rating': 0.8800000000000001,
+    'oneLiner': false
+  },
+  {
+    'joke': 'You are one well-defined function!'
+  },
+  {
+    'title': '',
+    'joke': 'Can you show me how to use the Internet?\nI\'d better - otherwise you\'ll just go round and round in circles.',
+    'category': 'Internet',
+    'rating': 1,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Teacher: Don\'t forget to check the Internet if you have trouble with your homework questions.\nPupil: It\'s not the questions I have trouble with, it\'s the answers.',
+    'category': 'Internet',
+    'rating': 0.8400000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Who sits at the end of the yellow brick road surfing the Net?\nThe www.izard of Oz.',
+    'category': 'Internet',
+    'rating': 0.8400000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Have you seen www.quasimodo.com?\nI\'m not sure, but certainly rings a bell.',
+    'category': 'Internet',
+    'rating': 0.8400000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Who is the most popular wizard on the Internet?\nHar e-potter.',
+    'category': 'Internet',
+    'rating': 0.8400000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Who looks after the EuroDisney website?\nMick e-mouse.',
+    'category': 'Internet',
+    'rating': 0.8400000000000001,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'Teacher: What are the four elements?\nPupil: Fire, Earth, Water and the Internet.\nTeacher: What do you mean the Internet?\nPupil: Well, Mum says that whenever I\'m on the Net, I\'m in my element.',
+    'category': 'Internet',
+    'rating': 0.8,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'What do you get if you cross a giant ship with the Internet?\nThe Site-anic.',
+    'category': 'Internet',
+    'rating': 0.8,
+    'oneLiner': false
+  },
+  {
+    'title': '',
+    'joke': 'What do you put in a www.ashing machine?\nNet curtains!',
+    'category': 'Internet',
+    'rating': 0.8,
+    'oneLiner': false
+  }
+
+];
+
+var jokes = jokeObjects.map((joke) => joke.joke);
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+};
+
+recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The [last] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+
+  var last = event.results.length - 1;
+  var color = event.results[last][0].transcript;
+
+  var jokeInd = Math.floor((jokes.length) * Math.random());
+
+  var nextJoke = jokes[jokeInd];
+
+  var utterThis = new SpeechSynthesisUtterance(nextJoke);
+  var selectedOption = synth.getVoices()[0];
+  
+  utterThis.pitch = 1;
+  utterThis.rate = .6;
+  synth.speak(utterThis);
+
+  utterThis.onpause = function(event) {
+    var char = event.utterance.text.charAt(event.charIndex);
+    console.log('Speech paused at character ' + event.charIndex + ' of "' +
+    event.utterance.text + '", which is "' + char + '".');
+
+  };
+
+
+
+
+
+};
+
+recognition.onspeechend = function() {
+  recognition.stop();
+};
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = 'I didn\'t recognise that color.';
+};
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+};
