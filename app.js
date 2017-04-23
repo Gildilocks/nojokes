965539514f4a403b7af69695fd5c77ac51eb621f
Dan Gilday <dangitday@gmail.com>
2017-04-22 18:30:19 -0700

    error handle

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ var static = require('node-static');
 // 
 // Create a node-static server instance to serve the './public' folder 
 // 
-var file = new static.Server('./public');
+var fileServer = new static.Server('./public');
  
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        // 
-        // Serve files! 
-        // 
-        file.serve(request, response);
+        fileServer.serve(request, response, function (err, result) {
+            if (err) { // There was an error serving the file 
+                console.error("Error serving " + request.url + " - " + err.message);
+ 
+                // Respond to the client 
+                response.writeHead(err.status, err.headers);
+                response.end();
+            }
+        });
     }).resume();
-}).listen(process.env.PORT || 3000);
+}).listen(8080);
