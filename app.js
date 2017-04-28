466997bc4ebce0038a7e82ab00fc1ef1e3c34f02
Dan Gilday <dangitday@gmail.com>
2017-04-28 10:06:13 -0700

    fix bug

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var static = require('node-static');
 // Create a node-static server instance to serve the './public' folder 
 // 
 var fileServer = new static.Server('./public');
-//var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
  
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
